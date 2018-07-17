f6f6aa90d30a97030592eb98e67afb64c4fc5aeb
dinsight <office.digital.insight@gmail.com>
2018-07-17 15:27:33 -0400

    Create MapCodes.cs

---
+++ tmp/MapCodes.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Map
+{
+    public class MapCodes
+    {
+        public static readonly int X = 1;
+        public static readonly int EXIT = 2;
+        public static readonly int CENTER = 3;
+        public static readonly int PADDING = 4;
+        public static readonly int PATH = 5;
+    }
+}
