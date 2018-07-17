abbb5f0c5ace65c45d75455f4c9cd8613fb02a27
dinsight <office.digital.insight@gmail.com>
2018-07-17 15:28:55 -0400

    Create Program.cs

---
+++ tmp/Program.cs
@@ -0,0 +1,76 @@
+using ConsoleApp2.Map;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int rows = 50;
+        static int cols = 50;
+
+        static void Main(string[] args)
+        {
+            var C = MapCodes.CENTER;
+            var E = MapCodes.EXIT;
+            var builder = new MapBuilder(rows, cols, 2, new PathFinder() );
+            var modules = new List<Module>();
+
+            var module1 = new Module(new int[,] {
+                { 0,0,1,1,E,1,1,1,0,0 },
+                { 0,1,1,1,1,1,1,1,1,0 },
+                { 1,1,1,1,1,1,1,1,1,1 },
+                { 1,1,1,1,1,C,1,1,1,1 },
+                { 0,0,0,1,1,1,1,1,1,1 },
+                { 0,0,0,1,1,1,1,1,1,0 },
+                { 0,0,0,1,E,1,1,1,1,0 },
+            });
+
+            var module2 = new Module(new int[,] {
+                { 0,0,0,0,0,E,0,0,0,0 },
+                { 0,0,0,0,1,1,1,0,0,0 },
+                { 0,0,1,1,1,1,1,1,1,0 },
+                { E,1,1,1,1,C,1,1,0,0 },
+                { 0,0,0,1,1,1,1,1,0,0 },
+                { 0,0,0,1,1,1,1,1,1,0 },
+                { 0,0,0,0,0,0,0,0,0,0 },
+            });
+
+            modules.Add(module1);
+            modules.Add(module2);
+            var roomCount = 30;
+            var actual = builder.Create(roomCount, modules);
+            Console.WriteLine($"Created {actual} out of {roomCount}");
+            PrintMap(builder);
+        }
+
+        private static void PrintMap(MapBuilder builder)
+        {
+            var buffer = new StringBuilder();
+            var line = new StringBuilder();
+            line.Append("    ");
+            for (int i = 0; i < cols; i++)
+            {
+                line.Append((i % 10).ToString("D1"));
+            }
+            buffer.Append(line.ToString());
+            buffer.AppendLine();
+            line.Length = 0;
+            line.Append("000 ");
+            builder.Apply((i, j, x) =>
+            {
+                line.Append(x == MapBuilder.Blank ? "*" : x.ToString());
+                if (j == cols - 1)
+                {
+                    line.AppendLine();
+                    buffer.Append(line.ToString());
+                    line.Length = 0;
+                    line.Append((i + 1).ToString("D3") + " ");
+                }
+            });
+            Console.WriteLine(buffer.ToString());
+            Console.ReadKey();
+        }
+    }
+}
