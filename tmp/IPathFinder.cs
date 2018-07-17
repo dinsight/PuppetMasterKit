26b67813d61c04c0442d7e439403568551b960bc
dinsight <office.digital.insight@gmail.com>
2018-07-17 15:26:47 -0400

    Create IPathFinder.cs

---
+++ tmp/IPathFinder.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pair = System.Tuple<int, int>;
+
+namespace ConsoleApp2.Map
+{
+    public class PathFinder : IPathFinder
+    {
+        public List<Pair> Find(int[,] map, int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            return null;
+        }
+    }
+}
