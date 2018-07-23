56d991c271bffcd52d406c20599648f4c935e5e6
dinsight <office.digital.insight@gmail.com>
2018-07-23 15:55:25 -0400

    Update Room.cs

--- PuppetMasterKit.AI/Map/Room.cs
+++ PuppetMasterKit.AI/Map/Room.cs
@@ -5,27 +5,28 @@ using Pair = System.Tuple<int, int>;
 namespace PuppetMasterKit.AI.Map
 {
   public class Room
-  {
-    Module module;
-    int row;
-    int col;
-
-    public int Id { get; set; }
-    public int PathCount { get; set; }
-    public int Row { get => row; set => row = value; }
-    public int Col { get => col; set => col = value; }
-    public Module Module { get => module; }
-
-    public Room(Module module, int row, int col)
     {
-      this.module = module;
-      this.row = row;
-      this.col = col;
-    }
+        private Module module;
+        private int row;
+        private int col;
+        private List<Pair> tiles;
 
-    internal IEnumerable<Pair> GetExits(int[,] map)
-    {
-      return module.GetExitsForPosition(map, Row, Col);
+        public int Id { get; set; }
+        public int PathCount { get; set; }
+        public int Row { get => row; set => row = value; }
+        public int Col { get => col; set => col = value; }
+        public Module Module { get => module; }
+
+        public Room(Module module, int[,] map, int row, int col) {
+            this.module = module;
+            this.row = row;
+            this.col = col;
+            this.tiles = module.GetTilesFromPositions(map, row, col).ToList();
+        }
+
+        public bool IsInside(int row, int col) 
+        {
+            return tiles.Any(x => x.Item1 == row && x.Item2 == col);
+        }
     }
-  }
 }
