f956d6536580f4073874ac349b173deb598d10b9
dinsight <office.digital.insight@gmail.com>
2019-07-17 09:12:48 -0400

    Update IMapGenerator.cs

--- PuppetMasterKit.Terrain/Map/IMapGenerator.cs
+++ PuppetMasterKit.Terrain/Map/IMapGenerator.cs
@@ -7,5 +7,7 @@ namespace PuppetMasterKit.Terrain.Map
   public interface IMapGenerator : I2DSubscript<int>
   {
     List<Region> Create(int rows, int cols);
+    
+    void UpdateFrom(Region region);
   }
 }
